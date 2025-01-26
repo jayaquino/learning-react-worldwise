1120f42f527cabe6ec04618d3b348abb48b0f0c8
Nelson Aquino Jr. <naquinojr@outlook.com>
2025-01-25 19:42:44 -0800

    feature(css): css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
 import PageNav from "../components/PageNav";
-
+import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Worldwise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
